c85f51aadd2e664f003baf535e3c9375ab7b74d5
Nikhil-Malladi <malladi.vsnikhil@gmail.com>
2022-03-19 15:11:45 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>I want to Deploy Flask to Circle CI</h1>'
+
+@app.route('/here')
+def here():
+    return '<h1>I Am Here! at Circle CI</h1>'
+
+
+if __name__=="__main__":
+    app.run()
